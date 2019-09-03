9bb00479ca7d20c76b619267a25cdfff7a4953b3
lavanyaananth <lavanyaanand.11@gmail.com>
2019-09-03 22:13:48 +0530

    fetch using G-books api

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,12 +3,18 @@ import {Link} from 'react-router-dom';
 
 
 class Dashboard extends Component{
-
+  componentDidMount() {
+    fetch('https://www.googleapis.com/books/v1/volumes?q=thealchemist')
+    .then(res => res.json())
+    .then((data) => console.log(data))
+    .catch(console.log)
+  }
   render(){
     return (
       <div className="Dashboard">
         <header className="App-header">
           <p> Book trading club</p>
+         
           <Link to="/users">View Users</Link>
           <Link to="/myprofile">Profile</Link>
         </header>
